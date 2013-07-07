0c9388f17094be9e73f08f8f91a94c6f15763d29
Ubuntu <ubuntu@ip-172-31-28-119.us-west-1.compute.internal>
2013-07-07 07:16:54 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World, Ainulindale!');
+  var buf = new Buffer(90);
+  fs.readFile('index.html', function(err, buf)) {
+    if (err) throw err;
+    console.log(buf);
+    var json = JSON.stringify(buf);
+    response.send(json);
+  }
 });
 
 var port = process.env.PORT || 5000;
